7d67e306eae0aa35314ea8005190647a1ff73c53
MarcosPatrickExe <marcospatrick039474@gmail.com>
2022-09-14 01:15:28 -0300

    cards estáticos e rodapé concluídos

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@
 import './styles/main.css';
 import logo from './assets/Logo.svg';
 import React from 'react';
+import { MagnifyingGlassPlus} from 'phosphor-react';
+
 // LINK DO FIGMA:
 // https://www.figma.com/file/AjotQe5dsfCyxCGjEY0iE2/NLW-eSports-(Community)?node-id=6%3A23
 
@@ -87,6 +89,22 @@ function App() {
             </div>
 
 
+            <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-1 overflow-hidden mx-16 mb-5">
+                <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center' >
+
+                    <div>
+                        <strong className='text-2xl text-white font-black block'>Não encontrou seu duo?</strong>
+                        <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos players! </span>
+                    </div>
+
+                    <button className="py-3 px-4 bg-violet-500 hover:bg-violet-700 text-white rounded flex items-center gap-3">
+                        
+                        <MagnifyingGlassPlus size={24} />
+                        Publicar anúncio
+                    </button>
+
+                </div>
+            </div>
 
         </div>
 
